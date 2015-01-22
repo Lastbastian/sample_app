6d3853307657b1d9c202b389da056b8031af4ed0
Chris Bastian <peacethrubeats@gmail.com>
2015-01-21 21:31:09 -0800

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -25,4 +25,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "#{@base_title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+
 end
